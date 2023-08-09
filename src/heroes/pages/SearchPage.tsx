c92038954b7cbae86b6026cfa2e41300f475cfb9
Paola Dl <kath.pao.acgmail.com>
2023-08-08 21:13:28 -0600

    search page desing

--- src/heroes/pages/SearchPage.tsx
+++ src/heroes/pages/SearchPage.tsx
@@ -1,7 +1,38 @@
+import { HeroCard } from "../components";
 export const SearchPage = () => {
   return (
-    <div>
-        SearchPage
+    <>
+    <h1>SearchPage</h1>
+    <hr />
+    <div className="row">
+    <div className="col-5">
+      <h4>Searching</h4>
+      <hr />
+      <form>
+        <input 
+          type="text"
+          placeholder="Search a Hero"
+          className="form-control"
+          name="searchText"
+          autoComplete="off"
+         />
+         <button type= "submit" className="btn btn-outline-primary mt 1">Search</button>
+      </form>
     </div>
+    <div  className="col-7">
+      <h4>Results</h4>
+        <hr />
+          <div className="alert alert-primary">
+            Search a Hero
+          </div>
+          <div className="alert alert-danger">
+            No Hero With <b>ABC</b>
+          </div>
+          
+          {/*<HeroCard  />*/}
+      </div>
+    </div>
+    
+    </>
   )
 }
