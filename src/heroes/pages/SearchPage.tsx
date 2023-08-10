c78de444a66575833ffa6df990cad6240488077a
Paola Dl <kath.pao.acgmail.com>
2023-08-09 21:46:43 -0600

    actualizacion en pagina de busqueda

--- src/heroes/pages/SearchPage.tsx
+++ src/heroes/pages/SearchPage.tsx
@@ -1,6 +1,27 @@
+import { useForm } from "../hooks";
 import { HeroCard } from "../components";
+import {  useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { getHeroesByName,} from "../helpers";
+import { Hero } from "..";
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const {q = ''} = queryString.parse(location.search);
+  const heroes: Hero[] = getHeroesByName(q as string);
+  console.log(heroes);
+  const { searchText, onInputChange } = useForm({
+    searchText: q
+  });
+
+  const onSearchSubmit = (event: React.FormEvent<HTMLFormElement> ) => {/*marca el lemento como un formulario html*/
+    event.preventDefault();
+    if ( searchText.trim().length <= 1) return;
+
+    navigate(`?q=${searchText.toLowerCase().trim()}`);
+  }
   return (
     <>
     <h1>SearchPage</h1>
@@ -9,13 +30,16 @@ export const SearchPage = () => {
     <div className="col-5">
       <h4>Searching</h4>
       <hr />
-      <form>
+      <form onSubmit={onSearchSubmit}>
         <input 
           type="text"
           placeholder="Search a Hero"
           className="form-control"
           name="searchText"
           autoComplete="off"
+          value={ searchText }
+          onChange={ onInputChange }
+
          />
          <button type= "submit" className="btn btn-outline-primary mt 1">Search</button>
       </form>
@@ -27,10 +51,18 @@ export const SearchPage = () => {
             Search a Hero
           </div>
           <div className="alert alert-danger">
-            No Hero With <b>ABC</b>
+            No Hero With <b>{q}</b>
           </div>
           
-          {/*<HeroCard  />*/}
+          {heroes.map(hero=>(
+            <HeroCard{...hero}/>
+          ))}
+          
+          {
+            heroes.map( hero => (
+              <HeroCard key={hero.id} {...hero}/>
+            ))
+          }
       </div>
     </div>
     
